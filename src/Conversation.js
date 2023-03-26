45bc0dbc12ef5559128dd27aacfc6a0ffa695d8c
jb6248 <jb6248@rit.edu>
2023-03-26 01:25:53 -0400

    add categories init

--- src/Conversation.js
+++ src/Conversation.js
@@ -21,7 +21,8 @@ class Conversation extends Component {
             age: [0, 0],
             ethnicities: [],
             genders: [],
-            states: []
+            states: [],
+            categories: []
         });
         this.state = {
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec dignissim purus, ut pretium dui. Etiam lobortis ultrices sodales. Etiam orci nulla, lacinia vel dui ultrices, tempor varius est. Donec commodo velit eu mi maximus mollis. Vivamus finibus orci maximus augue molestie, quis elementum ligula condimentum. Integer id hendrerit ipsum. Sed finibus dui ac imperdiet tempus. Nullam sit amet sagittis arcu, mattis posuere risus. In consequat efficitur augue eget pulvinar. Proin fringilla, urna id molestie facilisis, massa mauris sollicitudin massa, in convallis purus sem non metus. Etiam suscipit velit ut pretium iaculis. Nulla sed nisl dolor. Pellentesque aliquam sem non tortor finibus commodo. Nam non egestas nisl. Nunc lorem tellus, lobortis pellentesque commodo vitae, elementum ac nibh.",
